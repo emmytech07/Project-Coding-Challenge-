86ffa36a4da9149fecfe2e649bd490e3b199e6d0
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 14:11:30 +0100

    Pseudo Code

---
+++ 20 beginner Project/Random Password Generator/app.py
@@ -0,0 +1,5 @@
+# Ask a user if they want to generate a password or not 
+# If yes, ask for the password length,
+# Generate the password 
+# Print password 
+# If iniitial response is not, exist  the program
