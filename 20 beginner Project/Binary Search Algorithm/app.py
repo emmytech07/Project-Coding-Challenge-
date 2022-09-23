120274c101ac4819e6d7f223113875b6474eb1cd
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 08:34:37 +0100

    psuedocode

---
+++ 20 beginner Project/Binary Search Algorithm/app.py
@@ -0,0 +1,5 @@
+# function that takes a list and target parameter
+# Multiple variable: middle, start, end, steps
+# #recursion or while loop
+# increase the steps each time a plit is done 
+# conditions to track target position  
