2a60ed1c7da57edf38cdb914be6df28c2133ce78
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 10:54:32 +0100

    Qrcode images generated

--- 20 beginner Project/QR Code generator/app.py
+++ 20 beginner Project/QR Code generator/app.py
@@ -6,3 +6,20 @@
 # save the grcode as an image
 # run the function
 
+import qrcode
+def generate_qrcode(text):
+
+    qr =qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='black', back_color = 'white')
+    img.save("qrimgs.png")
+
+url = input("Enter yout url: ")
+generate_qrcode(url)
