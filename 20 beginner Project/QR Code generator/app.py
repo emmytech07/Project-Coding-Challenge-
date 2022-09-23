43960fdb3e2d03d29a9770a9e9273b7e00281b4c
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 10:28:47 +0100

    stating conditions

---
+++ 20 beginner Project/QR Code generator/app.py
@@ -0,0 +1,8 @@
+# from email.mime import image
+
+
+# Install all the libraries neeeded
+# create a function that collects a text and convert it to a qrcode
+# save the grcode as an image
+# run the function
+
