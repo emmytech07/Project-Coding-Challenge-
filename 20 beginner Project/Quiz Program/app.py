2d11b3c95f7e4505be04c9f8fb59cfb752932933
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 09:49:40 +0100

    Quiz Program

---
+++ 20 beginner Project/Quiz Program/app.py
@@ -0,0 +1,27 @@
+# Dictionary that stores questions and answer_challenge
+# have a variable that tracks the score of the year
+# loop thorugh the dictionary using the key values
+# display each question to the user and allow them to anwer
+# Tell them if they are right or wrong 
+# show the final result when quiz is completed
+
+quiz = {
+    "Questions 1": {
+        "Question" :"What is my age",
+        "Answer": 22
+    },
+    "Questions 2": {
+        "Question" :"What is my Name",
+        "Answer": "Hope"
+    },
+    "Questions 3": {
+        "Question" :"What is my School",
+        "Answer": "Joy International"
+    },
+    "Questions 4": {
+        "Question" :"What is my friend name",
+        "Answer": "Jesus"
+    }
+}
+
+score = 0
