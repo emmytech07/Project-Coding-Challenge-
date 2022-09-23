2808b24cface212d52ca5188f39f93afd101730a
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 03:39:08 +0100

    First Project

---
+++ 20 beginner Project/Word Replacement Program/app.py
@@ -0,0 +1,8 @@
+def replace_word():
+    str = "This is a coding challenge"
+    word_replace = input('Enter word to replace ')
+    word_replacement = input('Enter the word replacement ')
+
+    print(str.replace(word_replace, word_replacement))
+
+replace_word()
