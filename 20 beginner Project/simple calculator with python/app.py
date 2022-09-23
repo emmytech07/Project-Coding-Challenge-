a3e138cb4903001dcf531a2e52337f3059766fe0
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 04:08:29 +0100

    implementation with method 1

---
+++ 20 beginner Project/simple calculator with python/app.py
@@ -0,0 +1,27 @@
+def addition(a, b):
+    sum = a + b
+    print(f"{a} + {b} = {sum}")
+
+def subtraction(a, b):
+    sub = a + b
+    print(f"{a} - {b} = {sub}")
+
+def Multiplication(a, b):
+    mul = a * b
+    print(f"{a} * {b} = {mul}")
+
+def Division(a, b):
+    div = a / b
+    print(f"{a} - {b} / {div}")
+
+print("A: Addition")
+print("B: Subtraction")
+print("A: Multiplication")
+print("A: Division")
+
+choice = (input("Input your choice"))
+
+if choice == "A" // choice =='a':
+    print('You Option for Addition')
+    a = int()
+
