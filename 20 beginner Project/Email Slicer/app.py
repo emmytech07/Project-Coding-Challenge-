bf25c6fc0330e4e77cd7a6d5e77849d048247685
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 08:11:56 +0100

    Email Slicer project

---
+++ 20 beginner Project/Email Slicer/app.py
@@ -0,0 +1,11 @@
+def main():
+    print("This is a Email Slicer Project ")
+    print()
+    mail = input("Supply your mail: ")
+    (username, domain)= mail.split('@')
+    (domain, extension) = domain.split('.')
+
+    print(f"Username : {username}")
+    print(f"domain : {domain}")
+    print(f"Extension : {extension}")
+main()
