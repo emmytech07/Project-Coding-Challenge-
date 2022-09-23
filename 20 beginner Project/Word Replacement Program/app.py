245f717d5017d059ffb9c63048d4875d4eccbaa6
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 03:52:53 +0100

    Applying method 2 for project one

--- 20 beginner Project/Word Replacement Program/app.py
+++ 20 beginner Project/Word Replacement Program/app.py
@@ -1,8 +1,21 @@
-def replace_word():
-    str = "This is a coding challenge"
-    word_replace = input('Enter word to replace ')
-    word_replacement = input('Enter the word replacement ')
+# Method 1
+# def replace_word():
+#     str = "This is a coding challenge"
+#     word_replace = input('Enter word to replace ')
+#     word_replacement = input('Enter the word replacement ')
 
-    print(str.replace(word_replace, word_replacement))
+#     print(str.replace(word_replace, word_replacement))
 
-replace_word()
+# replace_word()
+
+# METHOD 2
+
+
+
+def replace_word(string):
+    word1 = str(input("Input your word to be replace in string: "))
+    word2 = str(input('Replaced with string: '))
+    
+    print(string.replace(word1, word2))
+
+replace_word("Possibilities is the order of the day")
