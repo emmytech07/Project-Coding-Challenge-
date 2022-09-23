1355c2100f9105e4726f50c2975602dd3f47a493
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 11:34:41 +0100

    Project completed

--- 20 beginner Project/Interest rate Monthly Calculator/app.py
+++ 20 beginner Project/Interest rate Monthly Calculator/app.py
@@ -1,3 +1,19 @@
 # Collect the necessary inputs: principal, annual interest rate, years
 # calculate the monthly payment 
 # output
+
+def main():
+    print("This is a monthly loan calculator")
+    print("")
+
+    principal = float(input("Supply loan amount: "))
+    apr = float(input("Supply Annual interest rate amount: "))
+    years =int (input("supply amount of years: "))
+
+    monthlyInterestRate = apr/1200
+    amountOfMonths = years * 12
+    monthlyPayment = principal * monthlyInterestRate / (1 - (1 + monthlyInterestRate) ** (-amountOfMonths))
+
+    print("Monthly payment is : %.2f" %monthlyPayment)
+
+main()
