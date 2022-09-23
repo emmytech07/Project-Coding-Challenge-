d46a2c10b1b0ab6924787c8cb60d32b9e79552e7
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 08:59:30 +0100

    Compiling ongoing

--- 20 beginner Project/Binary Search Algorithm/app.py
+++ 20 beginner Project/Binary Search Algorithm/app.py
@@ -3,3 +3,29 @@
 # #recursion or while loop
 # increase the steps each time a plit is done 
 # conditions to track target position  
+
+def SearchAlg(list, target):
+    start = 0
+    middle = 0
+    end = len(list)
+    count = 0
+
+    while(start <=end):
+        print(f"Counts {count} : str(list[start:end+1])")
+        
+        count = count + 1
+        middle = (middle + end) // 2
+
+        if(target == list[middle]):
+            return middle
+        if(target < list[middle]):
+            end = middle - 1
+        else:
+            start = middle + 1
+    
+    return -1
+
+mylist = [*range(10)]
+Element = 6
+
+SearchAlg(list, Element)
