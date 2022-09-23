8e534548904a33b40287bad53b0af62fe922ca0a
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 18:19:37 +0100

    setup

--- 20 beginner Project/Dice Rolling Simulator/app.py
+++ 20 beginner Project/Dice Rolling Simulator/app.py
@@ -2,3 +2,64 @@
 # Define a function to roll a dice
 # Create a dictionary that eill have the drawing of the dice 
 # Create a while asking user 
+import random
+
+def roll_dice():
+    DICE_ART = {
+    1: (
+        "┌─────────┐",
+        "│    1    │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    2     │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●   3  │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    4    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●  5 │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ● 6 ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+}
+    roll = input("Roll a dice? (Yes/No) ")
+
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+
+        print(f'Dice rolled: {dice1}, {dice2}')
+        print("\n".join(DICE_ART[dice1]))
+        print("\n".join(DICE_ART[dice2]))
+        # break
+        roll = input("Roll a dice? (Yes/No) ")
+
+
+roll_dice()
