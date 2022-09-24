54dfb36563fab91bc6600e6a9e58ef40f393259a
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-24 05:17:19 +0100

    adding Py-Dictionary

---
+++ 20 PYTHON PROJECTS/Word Dictionary/app1.py
@@ -0,0 +1,12 @@
+# Working with pyDictionary
+# pip install Py-Dictionary
+# dict = Dictionary("Tragedy")
+# meanings_list = dict.meanings()
+
+# synonyms_list = dict.synonyms()
+
+# antonyms_list = dict.antonyms()
+# print(meanings_list)
+# print(synonyms_list)
+
+from pydictionary import Dictionary
